2f991aaf66df1a228a41934f5dd04594d02acce9
ghost <minecraftghost90.ss@gmail.com>
2021-08-28 13:30:42 -0400

    Fixed .idea being blocked

--- src/com/company/MovieRanker.java
+++ src/com/company/MovieRanker.java
@@ -24,31 +24,34 @@ Your  program  should  loop,  accepting  input  and  producing  lists until at l
 public class MovieRanker {
 
     public static void main(String[] args) {
+        byte contin = 1;
+
         //File file = new File("../resource/asnlib/publicdata/ratings.tsv");
         File file = new File("ratings.tsv");
         ArrayList<MovieRating> rl = new ArrayList<MovieRating>();
 
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] tkns = line.split("\\t"); // tabs separate tokens
-                MovieRating nr = new MovieRating(tkns[0], tkns[1], tkns[2]);
-                rl.add(nr);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        do {
 
-        int minVotes = 1;
-        int numRecords = 1;
-        Scanner input = new Scanner(System.in);
-        while (true) {
+            try {
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] tkns = line.split("\\t"); // tabs separate tokens
+                    MovieRating nr = new MovieRating(tkns[0], tkns[1], tkns[2]);
+                    rl.add(nr);
+                }
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            int minVotes = 1;
+            int numRecords = 1;
+            Scanner input = new Scanner(System.in);
 
             MaxHeap<MovieRating> myHeap = new MaxHeap<MovieRating>();
 
-            for(int i = 0; i < rl.size(); i++) {
+            for (int i = 0; i < rl.size(); i++) {
                 myHeap.insert(rl.get(i));
             }
 
@@ -68,18 +71,30 @@ public class MovieRanker {
             int counter = 0;
             for (int i = 0; i < rl.size(); i++) {
                 MovieRating temp = myHeap.removemax();
-                if(temp.getVotes() > minVotes){
+                if (temp.getVotes() > minVotes) {
                     System.out.println(temp.toString());
                     counter++;
                 }
-                if(counter >= numRecords){
+                if (counter >= numRecords) {
                     break;
                 }
             }
 
             System.out.println();
             long readTime = System.currentTimeMillis();
-            System.out.println("Time: "+(System.currentTimeMillis()-startTime)+" ms");
-        }
+            System.out.println("Time: " + (System.currentTimeMillis() - startTime) + " ms");
+
+
+            System.out.println("Do you wish to rerun the ranker? 1 = yes, 0 = no.");
+            System.out.println();
+            contin = input.nextByte();
+
+            if(contin > 0)
+                System.out.println("You have selected to rerun the program.");
+            else
+                System.out.println("You have selected to stop the program. Have a nice day.");
+
+        } while(contin > 0);
+
     }
 }
